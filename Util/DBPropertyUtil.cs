e7f8945e05aa4ce65514d9c1b64a76d9fe5234dc
TejaswiniBhumireddy <106535392+TejaswiniBhumireddy@users.noreply.github.com>
2024-05-19 21:36:09 +0530

    Add files via upload

---
+++ Util/DBPropertyUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+public static class DBPropertyUtil
+{
+    public static string GetConnectionString(string propertyFileName)
+    {
+        try
+        {
+            string[] lines = File.ReadAllLines(propertyFileName);
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("connectionString="))
+                {
+                    return line.Substring("connectionString=".Length);
+                }
+            }
+
+            throw new Exception("Connection string not found in the property file.");
+        }
+        catch (FileNotFoundException)
+        {
+            throw new Exception("Property file not found.");
+        }
+        catch (IOException ex)
+        {
+            throw new Exception("Error reading property file: " + ex.Message);
+        }
+    }
+}
