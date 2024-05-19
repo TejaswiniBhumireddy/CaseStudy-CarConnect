594de1ff34cd8fd6a074d0c19b35af6dcede56bc
TejaswiniBhumireddy <106535392+TejaswiniBhumireddy@users.noreply.github.com>
2024-05-19 21:38:14 +0530

    Add files via upload

---
+++ CarConnectApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using CarConnect.CarConnectApp;
+using CarConnect.Util;
+using System.Data.SqlClient;
+using System;
+using CarConnect.Exceptions;
+using CarConnect.Model;
+using CarConnect.Service;
+
+namespace CarConnect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("------------Welcome to CarConnect-------------");
+            CarConnectApplication.MainMenu();
+        }
+    }
+}
+
+
